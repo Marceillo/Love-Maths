70ab19d05407a55df6f543816491d4d3149ac447
Marceillo De Wet <marceillo@hotmail.com>
2024-03-21 11:59:32 +0000

    Add code for subtract function

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     runGame("addition");
-})
+});
 /**
  * The main game "loop", called when the first loaded
  * and after the user answer has been processed
@@ -33,6 +33,8 @@ function runGame(gameType) {
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);  
 
+    } else if (gameType === "subtract" ) {
+        displaySubtractQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -77,6 +79,8 @@ function calculateCorrectAnswer() {
     } else if (operator === "x") {
               return [operand1 * operand2, "multiply"];  
 
+    }else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -111,7 +115,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
