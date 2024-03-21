e18104bb9a8d4245763b1c11817b73fde1d4411f
Marceillo De Wet <marceillo@hotmail.com>
2024-03-21 07:52:06 +0000

    Add code to calucalte two numbers for the game question

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+//Wait for DOM to finnesh before loading the game
+//Get the button elements and add the event liseners 
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+
+    }
+})
+/**
+ * The main game "loop", called when the first loaded
+ * and after the user answer has been processed
+ * 
+ */
+
+function runGame() {
+//Created two random number between 1 and 25
+let num1 = Math.floor(Math.random() * 25 + 1);
+let num2 = Math.floor(Math.random() * 25 + 1);
+}
+
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
